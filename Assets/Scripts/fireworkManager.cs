7dd3bd9a119a32fa4f5ab1ae1d703d8eb9f2dfc4
Kline Walker-Patrick <kwalkerp@hotmail.com>
2020-11-09 02:11:18 +0000

    Stage 2: Shoot fireworks using keys
    Now if the user presses SPACE, a firework will shoot and play the particle system

---
+++ Assets/Scripts/fireworkManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+public class fireworkManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    private VisualEffect fireWorkSystem;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            fireWorkSystem.SendEvent("OnPlay");
+        }
+    }
+}
