0b747c1637ee50cd4b79981c0df83c8097ebc18a
Kline Walker-Patrick <kwalkerp@hotmail.com>
2020-11-24 16:33:18 +0000

    Stage 3: Uses real-time audio analyst to fire rockets
    Can now fire the rockets whenever the audio reaches a certain threshold. However, next stage I will pre-process the audio

---
+++ Assets/Scripts/AudioSyncer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSyncer : MonoBehaviour
+{
+    public float bias; // Determines the threshold of the beat.
+    public float timeStep, timeToBeat, restSmoothTime;
+    [SerializeField]
+    private float previousAudioValue, currentAudioValue, timer;
+    [SerializeField]
+    protected bool isBeat;
+    
+    public virtual void OnBeat()
+    {
+        timer = 0;
+        isBeat = true;
+    }
+    public virtual void OnUpdate()
+    {
+        previousAudioValue = currentAudioValue;
+        currentAudioValue = AudioSpectrum.spectrumValue;
+        if(previousAudioValue > bias && currentAudioValue <= bias)
+        {
+            if(timer > timeStep)
+            {
+                OnBeat();
+            }
+        }
+        if(previousAudioValue <= bias && currentAudioValue > bias)
+        {
+            OnBeat();
+        }
+        timer += Time.deltaTime;
+    }
+    // Update is called once per frame
+    private void Update()
+    {
+        OnUpdate();
+    }
+}
---
+++ Assets/Scripts/AudioTrigger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+public class AudioTrigger : AudioSyncer
+{
+    [SerializeField]
+    private VisualEffect fireworkFX;
+    // Start is called before the first frame update
+    public override void OnBeat()
+    {
+        base.OnBeat();
+        fire();
+    }
+    // Update is called once per frame
+    public override void OnUpdate()
+    {
+        base.OnUpdate();
+        if (isBeat) return;
+        Debug.Log("Resting");
+    }
+
+    private void fire()
+    {
+        Debug.Log("fireMe");
+        fireworkFX.SendEvent("FireMain");
+        isBeat = false;
+    }
+}
