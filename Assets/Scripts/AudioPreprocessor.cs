7f13eca4dc8285c2a64f4ac7b8fd6ee340806386
Kline Walker-Patrick <kwalkerp@hotmail.com>
2020-11-27 18:54:03 +0000

    Stage 4:  Implemented a preprocessing algorithm for Audio
    Using the article from Jesse Scam(https://medium.com/giant-scam/algorithmic-beat-mapping-in-unity-preprocessed-audio-analysis-d41c339c135a) I can now pre-process the audio and generate a visual graph to show the beats in the file. 
    
    Next step would be reading the array and reacting to the indexes that have beats in them to trigger an event.

---
+++ Assets/Scripts/AudioPreprocessor.cs
@@ -0,0 +1,106 @@
+﻿/*The Code below belongs to the Jesse Scam (https://github.com/jesse-scam/algorithmic-beat-mapping-unity/blob/master/Assets/Lib/Internal/SpectralFluxAnalyzer.cs)
+ and is used purely for preprocessing the audio tracks. To see the article explaining the */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPreprocessorInfo
+{
+    public float time, spectralFlux, threshold, prunedSpectralFlux;
+    public bool isBeat;
+}
+
+public class AudioPreprocessor
+{
+    int numSamples = 1024;
+    float thresholdMultiplier = 1.5f;
+    int thresholdWindowSize = 50;
+    public List<AudioPreprocessorInfo> spectralFluxSamples;
+    float[] currSpec;
+    float[] prevSpec;
+    int indexToProcess;
+
+    public AudioPreprocessor()
+    {
+        spectralFluxSamples = new List<AudioPreprocessorInfo>();
+        indexToProcess = thresholdWindowSize / 2;
+        currSpec = new float[numSamples];
+        prevSpec = new float[numSamples];
+    }
+    public void setCurrSpectrum(float[] spectrum)
+    {
+        currSpec.CopyTo(prevSpec, 0);
+        spectrum.CopyTo(currSpec, 0);
+    }
+    float calculateRectifiedSpectralFlux()
+    {
+        float sum = 0f;
+        for(int i = 0; i < numSamples; i++)
+        {
+            sum += Mathf.Max(0f, currSpec[i] - prevSpec[i]);
+        }
+        return sum;
+    }
+    float getFluxThreshold(int spectralFluxIndex)
+    {
+        int windowStartIndex = Mathf.Max(0, spectralFluxIndex - thresholdWindowSize / 2);
+        int windowEndIndex = Mathf.Min(spectralFluxSamples.Count - 1, spectralFluxIndex + thresholdWindowSize / 2);
+        float sum = 0f;
+        for(int i = windowStartIndex;i < windowEndIndex; i++)
+        {
+            sum += spectralFluxSamples[i].spectralFlux;
+        }
+        float avg = sum / (windowEndIndex - windowStartIndex);
+        return avg * thresholdMultiplier;
+    }
+    float getPrunedSpectralFlux(int spectralFluxIndex)
+    {
+        return Mathf.Max(0f, spectralFluxSamples[spectralFluxIndex].spectralFlux - spectralFluxSamples[spectralFluxIndex].threshold);
+    }
+    bool isPeak(int spectralFluxIndex)
+    {
+        if (spectralFluxSamples[spectralFluxIndex].prunedSpectralFlux > spectralFluxSamples[spectralFluxIndex + 1].prunedSpectralFlux &&
+            spectralFluxSamples[spectralFluxIndex].prunedSpectralFlux > spectralFluxSamples[spectralFluxIndex - 1].prunedSpectralFlux) {
+            return true;
+        }
+        else { return false; }
+    }
+    public void analyzeSpectrum(float[] spectrum, float time)
+    {
+        setCurrSpectrum(spectrum);
+        AudioPreprocessorInfo currInfo = new AudioPreprocessorInfo();
+        currInfo.time = time;
+        currInfo.spectralFlux = calculateRectifiedSpectralFlux();
+        spectralFluxSamples.Add(currInfo);
+        if (spectralFluxSamples.Count >= thresholdWindowSize)
+        {
+            spectralFluxSamples[indexToProcess].threshold = getFluxThreshold(indexToProcess);
+            spectralFluxSamples[indexToProcess].prunedSpectralFlux = getPrunedSpectralFlux(indexToProcess);
+            int indextoDetectPeak = indexToProcess - 1;
+            bool currPeak = isPeak(indextoDetectPeak);
+            if (currPeak) { spectralFluxSamples[indextoDetectPeak].isBeat = true; }
+            indexToProcess++;
+        }
+        else { Debug.Log(string.Format("Not ready yet.  At spectral flux sample size of {0} growing to {1}", spectralFluxSamples.Count, thresholdWindowSize)); }
+
+    }
+
+    void logSample(int indexToLog)
+    {
+        int windowStart = Mathf.Max(0, indexToLog - thresholdWindowSize / 2);
+        int windowEnd = Mathf.Min(spectralFluxSamples.Count - 1, indexToLog + thresholdWindowSize / 2);
+        Debug.Log(string.Format(
+            "Peak detected at song time {0} with pruned flux of {1} ({2} over thresh of {3}).\n" +
+            "Thresh calculated on time window of {4}-{5} ({6} seconds) containing {7} samples.",
+            spectralFluxSamples[indexToLog].time,
+            spectralFluxSamples[indexToLog].prunedSpectralFlux,
+            spectralFluxSamples[indexToLog].spectralFlux,
+            spectralFluxSamples[indexToLog].threshold,
+            spectralFluxSamples[windowStart].time,
+            spectralFluxSamples[windowEnd].time,
+            spectralFluxSamples[windowEnd].time - spectralFluxSamples[windowStart].time,
+            windowEnd - windowStart
+        ));
+    }
+
+}
---
+++ Assets/Scripts/SongController.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using System.Numerics;
+using DSPLib;
+
+using Array = System.Array;
+public class SongController : MonoBehaviour
+{
+    int numChannels, numTotalSamples, sampleRate;
+    float clipLength;
+    float[] multiChannelSamples;
+    AudioPreprocessor audPP;
+    AudioSource aSource;
+    PlotController pController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        aSource = GetComponent<AudioSource>();
+        audPP = new AudioPreprocessor();
+        pController = GameObject.Find("PlotMachine").GetComponent<PlotController>();
+        multiChannelSamples = new float[aSource.clip.samples * aSource.clip.channels];
+        numChannels = aSource.clip.channels;
+        numTotalSamples = aSource.clip.samples;
+        clipLength = aSource.clip.length;
+        this.sampleRate = aSource.clip.frequency;
+        aSource.clip.GetData(multiChannelSamples, 0);
+		Debug.Log("GetData: Complete");
+        Thread bgThread = new Thread(this.getFullSpectrumThreaded);
+		bgThread.Start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int indexToPlot = getIndexFromTime(aSource.time) / 1024;
+        pController.updatePlot(audPP.spectralFluxSamples, indexToPlot);
+
+    }
+    public int getIndexFromTime(float curTime)
+    {
+        float lengthPerSample = this.clipLength / (float)this.numTotalSamples;
+
+        return Mathf.FloorToInt(curTime / lengthPerSample);
+    }
+
+    public float getTimeFromIndex(int index)
+    {
+        return ((1f / (float)this.sampleRate) * index);
+    }
+
+	public void getFullSpectrumThreaded()
+	{
+		try
+		{
+			// We only need to retain the samples for combined channels over the time domain
+			float[] preProcessedSamples = new float[this.numTotalSamples];
+
+			int numProcessed = 0;
+			float combinedChannelAverage = 0f;
+			for (int i = 0; i < multiChannelSamples.Length; i++)
+			{
+				combinedChannelAverage += multiChannelSamples[i];
+
+				// Each time we have processed all channels samples for a point in time, we will store the average of the channels combined
+				if ((i + 1) % this.numChannels == 0)
+				{
+					preProcessedSamples[numProcessed] = combinedChannelAverage / this.numChannels;
+					numProcessed++;
+					combinedChannelAverage = 0f;
+				}
+			}
+
+			Debug.Log("Combine Channels done");
+			Debug.Log(preProcessedSamples.Length);
+
+			// Once we have our audio sample data prepared, we can execute an FFT to return the spectrum data over the time domain
+			int spectrumSampleSize = 1024;
+			int iterations = preProcessedSamples.Length / spectrumSampleSize;
+
+			FFT fft = new FFT();
+			fft.Initialize((System.UInt32)spectrumSampleSize);
+
+			Debug.Log(string.Format("Processing {0} time domain samples for FFT", iterations));
+			double[] sampleChunk = new double[spectrumSampleSize];
+			for (int i = 0; i < iterations; i++)
+			{
+				// Grab the current 1024 chunk of audio sample data
+				Array.Copy(preProcessedSamples, i * spectrumSampleSize, sampleChunk, 0, spectrumSampleSize);
+
+				// Apply our chosen FFT Window
+				double[] windowCoefs = DSP.Window.Coefficients(DSP.Window.Type.Hanning, (uint)spectrumSampleSize);
+				double[] scaledSpectrumChunk = DSP.Math.Multiply(sampleChunk, windowCoefs);
+				double scaleFactor = DSP.Window.ScaleFactor.Signal(windowCoefs);
+
+				// Perform the FFT and convert output (complex numbers) to Magnitude
+				Complex[] fftSpectrum = fft.Execute(scaledSpectrumChunk);
+				double[] scaledFFTSpectrum = DSPLib.DSP.ConvertComplex.ToMagnitude(fftSpectrum);
+				scaledFFTSpectrum = DSP.Math.Multiply(scaledFFTSpectrum, scaleFactor);
+
+				// These 1024 magnitude values correspond (roughly) to a single point in the audio timeline
+				float curSongTime = getTimeFromIndex(i) * spectrumSampleSize;
+
+				// Send our magnitude data off to our Spectral Flux Analyzer to be analyzed for peaks
+				audPP.analyzeSpectrum(Array.ConvertAll(scaledFFTSpectrum, x => (float)x), curSongTime);
+			}
+
+			Debug.Log("Spectrum Analysis done");
+			Debug.Log("Background Thread Completed");
+
+		}
+		catch (System.Exception e)
+		{
+			// Catch exceptions here since the background thread won't always surface the exception to the main thread
+			Debug.Log(e.ToString());
+		}
+	}
+}
